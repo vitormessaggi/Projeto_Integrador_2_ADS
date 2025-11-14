ad46b3deb051dd03be0e40e2338c79f1f757888f
Vinicius Porcionato <123662367+ViniciusPorcionato@users.noreply.github.com>
2025-11-13 23:47:27 -0300

    Adicionado inicio do projeto de backend talisma

---
+++ Dev BackEnd/imoveisapi/src/main/java/br/com/talisma/api/imoveisapi/ImoveisapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.talisma.api.imoveisapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImoveisapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImoveisapiApplication.class, args);
+	}
+
+}
---
+++ Dev BackEnd/imoveisapi/src/test/java/br/com/talisma/api/imoveisapi/ImoveisapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.talisma.api.imoveisapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImoveisapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
